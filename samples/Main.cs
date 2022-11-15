f07e7582b1479d53bf850bfb53325ce6c5895d40
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-11-15 20:12:55 +0200

    Migrate project to .net6

--- samples/Main.cs
+++ samples/Main.cs
@@ -3,16 +3,16 @@
 
 using UIKit;
 
-namespace IDMPhotoBrowserSamples
+namespace IDMPhotoBrowserSamples;
+
+public class Application
 {
-    public class Application
+    // This is the main entry point of the application.
+    static void Main(string[] args)
     {
         // This is the main entry point of the application.
-        static void Main(string[] args)
-        {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
-        }
+        // If you want to use a different Application Delegate class from "AppDelegate"
+        // you can specify it here.
+        UIApplication.Main(args, null, typeof(AppDelegate));
     }
 }
--- samples/ViewController.cs
+++ samples/ViewController.cs
@@ -7,31 +7,30 @@ using UIKit;
 using Foundation;
 using System.Linq;
 
-namespace IDMPhotoBrowserSamples
+namespace IDMPhotoBrowserSamples;
+
+public partial class ViewController : UIViewController
 {
-    public partial class ViewController : UIViewController
+    protected ViewController(IntPtr handle) : base(handle) { }
+
+    public override void ViewDidLoad()
     {
-        protected ViewController(IntPtr handle) : base(handle) { }
+        base.ViewDidLoad();
 
-        public override void ViewDidLoad()
+        var imageUrls = new[]
         {
-            base.ViewDidLoad();
+            "https://farm4.static.flickr.com/3567/3523321514_371d9ac42f_b.jpg",
+            "https://farm4.static.flickr.com/3629/3339128908_7aecabc34b_b.jpg",
+            "https://farm4.static.flickr.com/3364/3338617424_7ff836d55f_b.jpg",
+            "https://farm4.static.flickr.com/3590/3329114220_5fbc5bc92b_b.jpg"
+        };
 
-            var imageUrls = new[]
-            {
-                "https://farm4.static.flickr.com/3567/3523321514_371d9ac42f_b.jpg",
-                "https://farm4.static.flickr.com/3629/3339128908_7aecabc34b_b.jpg",
-                "https://farm4.static.flickr.com/3364/3338617424_7ff836d55f_b.jpg",
-                "https://farm4.static.flickr.com/3590/3329114220_5fbc5bc92b_b.jpg"
-            };
+        var photos = imageUrls.Select(imageUrl => new IDMPhoto(NSUrl.FromString(imageUrl))).ToArray();
+        var browser = new IDMPhotoBrowser(photos);
 
-            var photos = imageUrls.Select(imageUrl => new IDMPhoto(NSUrl.FromString(imageUrl))).ToArray();
-            var browser = new IDMPhotoBrowser(photos);
-
-            OpenBrowserBtn.TouchUpInside += (sender, e) =>
-            {
-                PresentViewController(browser, true, () => { });
-            };
-        }
+        OpenBrowserBtn.TouchUpInside += (sender, e) =>
+        {
+            PresentViewController(browser, true, () => { });
+        };
     }
 }
